2d05a8bdbdf0fdcd58fb0d85268a05c431aa4806
LarisaGimbu <gimbularisa@gmail.com>
2021-12-17 15:35:57 +0100

    text length

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php 
+/*
+1) Creare una variabile con un paragrafo di testo a vostra scelta.
+2) Stampare a schermo il paragrafo e la sua lunghezza.
+3) Una parola da censurare viene passata dall’utente tramite parametro GET.
+4) Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+*/
 
+$text = 'Piove sulle tamerici salmastre ed arse, piove sui pini scagliosi ed irti piove su i mirti divini.'
 
 
 ?>
@@ -14,6 +21,8 @@
   <title>PHP-badwords</title>
 </head>
 <body>
-  
+  <h2>Il testo originale:</h2>
+  <p><?php echo $text ?></p>
+  <p>Il testo è lungo: <?php echo strlen($text)?> caratteri</p>
 </body>
 </html>
